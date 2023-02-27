8e6a46c0b392275f246d88b9098b4d1bed6fd45d
Rasha <badranrasha685@gmail.com>
2023-02-27 14:39:15 +0200

    finish all the controllers

--- LMS-Backend/app/Models/LevelSection.php
+++ LMS-Backend/app/Models/LevelSection.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use AjCastro\EagerLoadPivotRelations\EagerLoadPivotTrait;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class LevelSection extends Pivot
+{   
+    
+    use EagerLoadPivotTrait;
+
+    protected $fillable = [
+        'capacity',
+        ];
+
+    //you only but attendance and userlevelsection relation here
+    
+}
--- LMS-Backend/app/Models/Section.php
+++ LMS-Backend/app/Models/Section.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use AjCastro\EagerLoadPivotRelations\EagerLoadPivotTrait;
+
+class Section extends Model
+{
+  use EagerLoadPivotTrait;
+
+    protected $fillable = [
+        'sectionName',
+        
+    ];
+    public function levels()
+    {
+      return $this->belongsToMany(Level::class, 'level_sections', 'level_id', 'section_id');
+     
+    }
+
+   
+}
