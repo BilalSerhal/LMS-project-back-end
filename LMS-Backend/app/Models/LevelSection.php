9de91f9c35a593ecf0e9fa834e64ff66d05d5948
Rasha <badranrasha685@gmail.com>
2023-02-27 13:07:16 +0200

    updates

--- LMS-Backend/app/Models/LevelSection.php
+++ LMS-Backend/app/Models/LevelSection.php
@@ -1,18 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class LevelSection extends Model
-{
-    use HasFactory;
-    public function Level(){
-        return $this->belongsToMany(Level::class);
-    }
-
-    public function Section(){
-        return $this->belongsToMany(Section::class);
-    }
-}
--- LMS-Backend/app/Models/Section.php
+++ LMS-Backend/app/Models/Section.php
@@ -1,14 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Section extends Model
-{
-    use HasFactory;
-    public function levelSection(){
-        return $this->hasMany(LevelSection::class);
-    }
-}
