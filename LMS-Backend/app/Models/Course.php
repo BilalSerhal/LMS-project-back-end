83037325dab0bc3609eb9403cfbc0f55fc04add6
Raneem <aljamal.raneem@gmail.com>
2023-03-02 20:40:34 +0200

    commit

--- LMS-Backend/app/Models/Course.php
+++ LMS-Backend/app/Models/Course.php
@@ -8,15 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-    protected $fillable = [
-        'subject',
-        ];
-=======
     use HasFactory,SoftDeletes;
    
->>>>>>> origin/dev
 
     protected $fillable = [
         'subject',
